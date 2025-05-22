4567ada6369144990c4b65d9fcfc4fe0c7b7452c
leegunho <lkh0239@naver.com>
2025-05-22 17:55:35 +0900

    meterialDesign 패키지 추가

---
+++ CollaborationTools/TeamCalendar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CollaborationTools;
+
+public partial class TeamCalendar : UserControl
+{
+    public TeamCalendar()
+    {
+        InitializeComponent();
+    }
+}
