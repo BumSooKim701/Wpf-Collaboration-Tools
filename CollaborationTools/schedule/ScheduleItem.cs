e7bec3d5bb07b8bd503b7187b3cbb6b960cefb52
leegunho <lkh0239@naver.com>
2025-05-30 19:16:22 +0900

    calendar widget and schedule list

---
+++ CollaborationTools/schedule/ScheduleItem.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace CollaborationTools;
+
+public class ScheduleItem : INotifyPropertyChanged
+{
+    private string _scheduleTitle;
+    public string scheduleTitle
+    {
+        get { return _scheduleTitle; }
+        set
+        {
+            if (_scheduleTitle != value)
+            {
+                _scheduleTitle = value;
+                NotifyPropertyChanged(nameof(scheduleTitle));
+            }
+        }
+    }
+
+    private string _scheduleDate;
+    public string scheduleDate
+    {
+        get { return _scheduleDate; }
+        set
+        {
+            if (_scheduleDate != value)
+            {
+                _scheduleDate = value;
+                NotifyPropertyChanged(nameof(scheduleDate));   
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    private void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ CollaborationTools/schedule/TeamCalendar.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace CollaborationTools;
+
+public partial class TeamCalendar : UserControl
+{
+    private ObservableCollection<ScheduleItem> schedules = new ObservableCollection<ScheduleItem>();
+    public TeamCalendar()
+    {
+        InitializeComponent();
+        
+        LoadScheduleItems();
+    }
+
+    private void LoadScheduleItems()
+    {
+        schedules.Add(new ScheduleItem { scheduleTitle = "미팅", scheduleDate = "2024-03-20" });
+        schedules.Add(new ScheduleItem { scheduleTitle = "프로젝트 마감", scheduleDate ="2024-03-21" });
+        schedules.Add(new ScheduleItem { scheduleTitle = "휴가", scheduleDate = "2024-03-22" });
+
+        CardListView.ItemsSource = schedules;
+    }
+}
