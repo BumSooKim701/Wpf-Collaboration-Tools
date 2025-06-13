42a56d27a93d3f89dd127a59785af14480fcfd31
leegunho <lkh0239@naver.com>
2025-06-13 11:49:20 +0900

    업데이트 후 커밋

--- CollaborationTools/obj/Debug/net8.0-windows/CollaborationTools.AssemblyInfo.cs
+++ CollaborationTools/obj/Debug/net8.0-windows/CollaborationTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollaborationTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bde2f86a34e0becb7339120f8915804cb72070b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e56c66311f6e5a911193688c29eadf5b5a99db8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollaborationTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollaborationTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
